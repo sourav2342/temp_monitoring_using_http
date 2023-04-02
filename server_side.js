92f9d131db12800ecad6c664c941355961f2e794
sourav2342 <104992048+sourav2342@users.noreply.github.com>
2023-04-02 22:12:28 +0530

    Update server_side.js

--- server_side.js
+++ server_side.js
@@ -113,6 +113,6 @@ const server = http.createServer((req, res) => {
 
 
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(443, () => {
+  console.log('Server listening on port 443');
 });
